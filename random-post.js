d2b638fb31a51fa927857f066a1cbcaf284fad66
cahendi <cahendi@gmail.com>
2017-05-01 22:57:31 +0700

    Create random-post.js

---
+++ random-post.js
@@ -0,0 +1,30 @@
+<style type="text/css">
+/*widget*/
+#rp_plus_img{overflow:hidden;border:solid 1px #ddd;padding:10px; background-color:none;line-height:1.7em;}
+#rp_plus_img ul{list-style-type:none;margin:0;padding:0;}
+#rp_plus_img li{border:0; margin:0; padding:0; list-style:none;}
+#rp_plus_img li{height:70px;padding:1px;list-style:none;}
+#rp_plus_img a{color:#3366ff;}
+#rp_plus_img .news-title{display:block;font-weight:normal !important;margin-bottom:5px;font-size:14px;}
+#rp_plus_img .news-text{display:block;font-size:12px;font-weight:normal !important;color:#282828;text-align:justify;}
+#rp_plus_img img{float:left;margin-right:10px;padding:1px;border:solid 1px #cccccc;width:60px;height:60px;border-radius:4px;}
+</style>
+<!-- start -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdn.rawgit.com/Brando07/share/master/random-post-bergerak-seocips.js"></script>
+<script type="text/javascript">
+var speed = 400;
+var pause = 3500;
+$(document).ready(function(){
+rpnewsticker();
+interval = setInterval(rpnewsticker, pause);
+});
+</script>
+<ul id="rp_plus_img">
+<script style="text/javascript">
+var numposts = 5;
+var numchars = 0;
+</script>
+<script src="http://bloging-gampang.blogspot.com/feeds/posts/default?orderby=published&alt=json-in-script&callback=rpthumbnt"></script>
+</ul>
+<!-- /end --> 
